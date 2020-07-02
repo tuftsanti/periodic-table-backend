ff217068f004b84f46e75f1ae690f98a512896fc
Andy Riecker <ariecker@gmail.com>
2020-07-02 09:42:14 -0400

    seed file from file

---
+++ elements.js
@@ -0,0 +1,30 @@
+const elements = 
+[
+  {
+    id: 1,
+    name: 'Charity Ball',
+    category: 'Fundraising',
+    description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+    featuredImage: 'https://placekitten.com/500/500',
+    images: [
+      'https://placekitten.com/500/500'
+    ],
+    location: '1234 Fancy Ave',
+    date: '12-25-2019',
+    time: '11:30'
+  },
+  {
+    id: 2,
+    name: 'Rescue Center Goods Drive',
+    category: 'Adoptions',
+    description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
+    featuredImage: 'https://placekitten.com/500/500',
+    images: [
+      'https://placekitten.com/500/500'
+    ],
+    location: '1234 Dog Alley',
+    date: '11-21-2019',
+    time: '12:00'
+  }
+];
+module.exports = elements
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+const elements = require('./elements.js')
+
 const authConfig = {
     domain: "ariecker.us.auth0.com",
     audience: "https://express-api"
@@ -32,37 +34,35 @@ issuer: `https://${authConfig.domain}/`,
 algorithms: ["RS256"]
 });
 
-const elements = 
-[
-  {
-    id: 1,
-    name: 'Charity Ball',
-    category: 'Fundraising',
-    description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
-    featuredImage: 'https://placekitten.com/500/500',
-    images: [
-      'https://placekitten.com/500/500',
-      'https://placekitten.com/500/500',
-      'https://placekitten.com/500/500',
-    ],
-    location: '1234 Fancy Ave',
-    date: '12-25-2019',
-    time: '11:30'
-  },
-  {
-    id: 2,
-    name: 'Rescue Center Goods Drive',
-    category: 'Adoptions',
-    description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
-    featuredImage: 'https://placekitten.com/500/500',
-    images: [
-      'https://placekitten.com/500/500'
-    ],
-    location: '1234 Dog Alley',
-    date: '11-21-2019',
-    time: '12:00'
-  }
-];
+// const elements = 
+// [
+//   {
+//     id: 1,
+//     name: 'Charity Ball',
+//     category: 'Fundraising',
+//     description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+//     featuredImage: 'https://placekitten.com/500/500',
+//     images: [
+//       'https://placekitten.com/500/500'
+//     ],
+//     location: '1234 Fancy Ave',
+//     date: '12-25-2019',
+//     time: '11:30'
+//   },
+//   {
+//     id: 2,
+//     name: 'Rescue Center Goods Drive',
+//     category: 'Adoptions',
+//     description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
+//     featuredImage: 'https://placekitten.com/500/500',
+//     images: [
+//       'https://placekitten.com/500/500'
+//     ],
+//     location: '1234 Dog Alley',
+//     date: '11-21-2019',
+//     time: '12:00'
+//   }
+// ];
 
 app.get('/elements', (req, res) => {
     // res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict"),
